01c4f7a3dcbe8f89d053953f9e2b4147d1270a12
andela-oawofadeju <olayemi.awofadeju@andela.com>
2017-01-15 20:37:30 +0100

    add getIndex method

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,1 +1,20 @@
-var 3hello = "4".
+var index = function() {
+  var key;
+  var word;
+
+  this.objectFiles = {};
+  this.invertedIndex = {};
+
+  this.getIndex = function() {
+    for (key in file) {
+      text = file[key].toLowerCase().replace(/\W+/g, ' ').trim().split(' ');
+      for (word = 0; word < text.length; word++) {
+        if () {};
+      }
+    }
+    this.createIndex = function(souceFile) {
+
+    }
+  }
+
+};
