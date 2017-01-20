1291b61aec25b9caa87ed75d7e771db3a3906733
andela-oawofadeju <olayemi.awofadeju@andela.com>
2017-01-20 05:09:10 +0100

    implement CreateIndex features

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,23 +1,51 @@
-var index = function() {
-  var key;
-  var word;
+var books = [{
+    "title": "Alice in Wonderland",
+    "text": "Alice falls into a rabbit hole and enters a world full of imagination."
+  },
 
-  this.objectFiles = {};
-  this.invertedIndex = {};
-
-  this.getIndex = function() {
-    for (key in file) {
-      text = file[key].toLowerCase().replace(/\W+/g, ' ').trim().split(' ');
-      for (word = 0; word < text.length; word++) {
-        if () {};
-      }
-    }
-    this.createIndex = function(sourceFile) {
-      if (sourceFile) {
-        var dataValue = this.invertedIndex.getfile(sourceFile);
-      }
-
-    }
+  {
+    "title": "The Lord of the Rings: The Fellowship of the Ring.",
+    "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
   }
+];
+//create an inverted index class;
+class invertedIndex {
+  constructor() {
+    this.indices = [];
+  }
+  tokenizer(string) {
+      return string.replace(/[^a-z\d\s]/ig, '').split(/\s+/);
+    }
+    //implement a createIndex method
+  createIndex(filename, content) {
+    const result = {};
+    let doc = 1;
+    content.forEach(book => {
+      for (let key in book) {
+        this.tokenizer(book[key]).forEach(word => {
+          if (!result.hasOwnProperty(word)) {
+            result[word] = []
+          }
+          if (result[word].indexOf(doc) > -1) {
+            return;
+          }
+          result[word].push(doc);
+        });
+      }
+      doc++;
+    });
+    console.log(result);
+    this.indices[books] = result;
+  };
 
-};
+  getIndex(fileName) {
+    return this.indices[fileName];
+  };
+  searchIndex(fileName, query) {
+
+  }
+}
+
+const indexer = new invertedIndex();
+indexer.createIndex('book', books);
+indexer.getIndex('book', books);
