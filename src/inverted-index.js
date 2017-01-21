21c06e883e941111568343083c3338542c7b2e30
andela-oawofadeju <olayemi.awofadeju@andela.com>
2017-01-21 18:01:04 +0100

    add the get and search method

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,30 +1,44 @@
 var books = [{
     "title": "Alice in Wonderland",
-    "text": "Alice falls into a rabbit hole and enters a world full of imagination."
+    "text": "Alice falls into elf a rabbit, hole. and enters a world full of imagination.",
   },
 
   {
     "title": "The Lord of the Rings: The Fellowship of the Ring.",
-    "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
+    "text": "An alice unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring. imagination"
   }
 ];
-//create an inverted index class;
+var bookTwo = [{
+    "title": "Alice in Wonderland",
+    "text": "and enters a world full of imagination.",
+  },
+
+  {
+    "title": "Hunger games The Fellowship of the Ring.",
+    "text": "A game of life an death"
+  }
+];
 class invertedIndex {
+
   constructor() {
     this.indices = [];
   }
+
   tokenizer(string) {
-      return string.replace(/[^a-z\d\s]/ig, '').split(/\s+/);
-    }
-    //implement a createIndex method
-  createIndex(filename, content) {
+    return string.replace(/[^a-z\d\s]/ig, '')
+      .toLowerCase()
+      .split(/\s+/);
+  }
+
+  createIndex(fileName, content) {
     const result = {};
     let doc = 1;
     content.forEach(book => {
       for (let key in book) {
+        // console.log(this.tokenizer(book[key]))
         this.tokenizer(book[key]).forEach(word => {
           if (!result.hasOwnProperty(word)) {
-            result[word] = []
+            result[word] = [];
           }
           if (result[word].indexOf(doc) > -1) {
             return;
@@ -34,18 +48,29 @@ class invertedIndex {
       }
       doc++;
     });
-    console.log(result);
-    this.indices[books] = result;
-  };
+    this.indices[fileName] = result;
+  }
 
   getIndex(fileName) {
     return this.indices[fileName];
-  };
-  searchIndex(fileName, query) {
+  }
 
+  searchIndex(fileName, query) {
+    const result = {};
+    const indexedFile = this.getIndex(fileName);
+    if (!query || !indexedFile) {
+      return "file dose not exist";
+    }
+    this.tokenizer(query).forEach(word => {
+      if (indexedFile.hasOwnProperty(word)) {
+        result[word] = indexedFile[word];
+      }
+    });
+    return result;
   }
 }
-
 const indexer = new invertedIndex();
 indexer.createIndex('book', books);
+indexer.createIndex('bookTwo', bookTwo);
 indexer.getIndex('book', books);
+console.log(indexer.searchIndex('book', "Alice"));
