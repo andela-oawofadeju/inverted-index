79a1cd08c6e613018225909465eeef2a4f6bce19
andela-oawofadeju <olayemi.awofadeju@andela.com>
2017-01-23 21:36:46 +0100

    for review

--- src/app.js
+++ src/app.js
@@ -1,12 +1,4 @@
 "use strict";
 //Angular module for inverted index
 
-var indexer = angular.module('invertedIndex', []);
-indexer.directive('fileUpload', function() {
-  var indexer = {};
-  directive.require = "ngModel";
-
-}).
-run(function() {
-
-});
+angular.module('InvertedIndex', []);
