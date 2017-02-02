626dd0bbc0f666924fa0ba1b261712f6d8cc0b58
andela-oawofadeju <olayemi.awofadeju@andela.com>
2017-02-02 08:53:56 +0100

    modify file for testing

--- jasmine/books.js
+++ jasmine/books.js
@@ -1,11 +1,4 @@
-// const fs = require('fs');
-// const validbook = fs.readFileSync('../books.json');
-// // const emptyBook = fs.readFileSync('../books copy.json');
-// // const invalidBook = fs.readFileSync('../books copy 2.json');
-
-
-const validBook
- = [{
+const validBook = [{
     "title": "Alice in Wonderland",
     "text": "Alice falls into a rabbit hole and enters a world full of imagination."
   },
@@ -14,11 +7,11 @@ const validBook
     "title": "The Lord of the Rings: The Fellowship of the Ring.",
     "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
   }
-]
+];
 
 const invalidBook = [{
   "name": "alice in Wonderland",
   "content": "this is crazy"
-}]
+}];
 
-const emptyBook = []
+const emptyBook = [];
