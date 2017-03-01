1c1500a4ff93f3128c4d4706f1aaa8e25d17bb23
andela-oawofadeju <olayemi.awofadeju@andela.com>
2017-03-01 17:47:23 +0100

    browserify(gulp): bundle and browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
 
 gulp.task('browser-sync', () => {
   browserSync.init({
@@ -11,9 +14,38 @@ gulp.task('browser-sync', () => {
   });
 });
 
+
+gulp.task('scripts', () => {
+  gulp.src('jasmine/spec/inverted-index-test.js')
+    .pipe(browserify())
+    .pipe(rename('bundle.js'))
+    .pipe(gulp.dest('jasmine/build'));
+});
+
+gulp.task('browserify', () => {
+  return browserify('jasmine/spec/inverted-index-test.js')
+    .bundle()
+    .pipe(source('browser-test.spec.js'))
+    .pipe(gulp.dest('jasmine/build'));
+});
+
+
+
+gulp.task('browserTest', ['scripts'], () => {
+  testBrowserSync.init({
+    server: {
+      baseDir: ['./src/js/', './jasmine'],
+      index: 'SpecRunner.html'
+    },
+    port: 8080,
+    ui: false,
+    ghostMode: false
+  });
+});
+
 gulp.task('watch', () => {
-  gulp.watch(['./**/*.html', 'src/css/*.css', 'src/js/*.js'])
+  gulp.watch(['./**/*.html', 'src/css/*.css', 'src/js/*.js', './jasmine/spec/*.js'])
     .on('change', browserSync.reload);
 });
 
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', ['browserTest', 'browser-sync', 'watch']);
